5cf0fb38c84d5ab80c6d408e61928fc6240aa199
piyushrastogi-17 <piyushrastogi7505@gmail.com>
2026-09-04 14:22:58 +0530

    project setup

---
+++ Backend/services/auth/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import dotenv from "dotenv"
+import { connectDB } from "./configs/db.js"
+dotenv.config()
+
+const app = express()
+
+const PORT = process.env.PORT || 6001
+
+app.get("/" , (req,res) => {
+    res.send("Hello from Auth-service")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Auth-service Started on ${PORT}`)
+    connectDB()
+})
