fd357508d21118cecc467d7e1aef7ac2da94aead
piyushrastogi-17 <piyushrastogi7505@gmail.com>
2026-09-06 12:18:19 +0530

    Add Google authentication

---
+++ Backend/services/auth/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+import { app } from "../configs/firebase.js";
+import {getAuth} from "firebase-admin/auth";
+import User from "../models/user.model.js";
+import crypto from "crypto";
+
+export const GoogleAuth = async (req, res) => {
+    try {
+        const {token} = req.body;
+        
+        const decoded = await getAuth(app).verifyIdToken(token)
+
+        let user = await User.findOne({
+            firebaseUid:decoded.uid
+        })
+
+        if(!user) {
+            user = await User.create({
+                firebaseUid:decoded.uid,
+                name:decoded.name,
+                email:decoded.email 
+            })
+        }
+
+
+        const sessionId = crypto.randomUUID()
+
+        res.cookies("session", sessionId , {
+            httponly:true,
+            secure:false,
+            sameSite:"strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return res.status(200).json({success:true , user})
+
+
+
+
+
+    } catch (error) {
+        return res.status(500).json("Google Auth Error", error)
+    }
+}
