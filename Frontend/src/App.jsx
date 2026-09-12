e808e1beb20d188b78e58d21109bcf8f4b84251a
piyushrastogi-17 <piyushrastogi7505@gmail.com>
2026-09-12 12:59:16 +0530

    Google Authentication using firebase

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/dashboard' element={<Dashboard/>} />
+
+        </Routes>
+    </>
   )
 }
 
---
+++ Frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Dashboard() {
+    return (
+        <div className='text-green-200 text-3xl'>
+            Dashboard Page
+        </div>
+    )
+}
+
+export default Dashboard
