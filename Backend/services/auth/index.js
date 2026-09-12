54d263b86f8a783cf39f44f9c59a886f3d99532d
piyushrastogi-17 <piyushrastogi7505@gmail.com>
2026-09-12 13:07:01 +0530

    authentication configuration setup

--- Backend/services/auth/index.js
+++ Backend/services/auth/index.js
@@ -1,15 +1,24 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./configs/db.js"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js";
 dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(cookieParser()) 
 
 const PORT = process.env.PORT || 6001
 
 app.get("/" , (req,res) => {
     res.send("Hello from Auth-service")
 })
+
+
+
+app.use("/", authRouter)
 
 app.listen(PORT, ()=> {
     console.log(`Auth-service Started on ${PORT}`)
